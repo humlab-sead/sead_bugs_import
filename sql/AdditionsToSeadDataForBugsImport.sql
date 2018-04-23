75bddeb85e21ae33a03f285cb90e644b8e2375d5
Erik Eriksson <erik.j.eriksson@umu.se>
2018-04-23 16:27:37 +0200

    Added needed scripts for data manipulation in conjuction with the import.

---
+++ sql/AdditionsToSeadDataForBugsImport.sql
@@ -0,0 +1,36 @@
+﻿ALTER TABLE tbl_taxa_measured_attributes
+   ALTER COLUMN attribute_type TYPE character varying(255);
+ALTER TABLE tbl_taxa_measured_attributes
+   ALTER COLUMN attribute_measure TYPE character varying(255);
+
+insert into tbl_species_association_types (association_type_name) values ('dung in burrows parasitised by');
+insert into tbl_species_association_types (association_type_name) values ('found in same log as');
+insert into tbl_species_association_types (association_type_name) values ('found breeding in same log as');
+insert into tbl_species_association_types (association_type_name) values ('in same habitat as?');
+insert into tbl_species_association_types (association_type_name) values ('in same habitat, but not in burials, as');
+insert into tbl_species_association_types (association_type_name) values ('in same pools as');
+insert into tbl_species_association_types (association_type_name) values ('in same upland habitat as');
+insert into tbl_species_association_types (association_type_name) values ('in same upland pools as');
+insert into tbl_species_association_types (association_type_name) values ('in same wood as');
+insert into tbl_species_association_types (association_type_name) values ('is kleptoparasitic in burrows of');
+insert into tbl_species_association_types (association_type_name) values ('is kleptoparasitic in nests of');
+insert into tbl_species_association_types (association_type_name) values ('is kleptoparasitic on');
+insert into tbl_species_association_types (association_type_name) values ('is kleptoparasitised by');
+insert into tbl_species_association_types (association_type_name) values ('is loosely associated with');
+insert into tbl_species_association_types (association_type_name) values ('predated on by');
+
+insert into tbl_dating_labs (international_lab_id, lab_name) values ('Unknown', 'Unknown or unspecified');insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Cal',20,'Calibrated radiocarbon date (method unspecified)',null,8,'Calendar years date provided by the calibration of a radiocarbon age. Exact dating method unspecified.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('CalAMS',20,'Calibrated AMS radiocarbon date',null,8,'Calendar years date provided by calibration of an AMS radiocarbon age.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('GeolPer',19,'Geological period (unspecified years)',null,15,'Geological period with unspecified years type');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Interp 14C',3,'Interpolated C14','19',7,'Dating using the interpolation of 14C dates from other samples (give details in dating notes)');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('ESR',21,'Electron Spin Resonance','19',15,'Dating based on the trapped charges of electrons, electron spin resonance dating.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('He-U',3,'Helium-Uranium','19',15,'Dating based on the production of helium during the radioactive decay of uranium and thorium.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Cl36',3,'Chlorine 36','19',15,'Dating based on the radioactive decay of chlorine 36');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('U-Trend',3,'Uranium trend','19',15,'Dating utilizing disequilibrium in the decay of Uranium-238, Uranium-234 and Thorium 230.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Be10',3,'Berylium 10','19',15,'Dating based on the radioactive decay of berylium 10.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('K-Ar',3,'Potassium-Argon','19',15,'Dating based on the radioactive decay of potassium into argon.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Orb-Tuning',21,'Orbital Tuning','19',15,'Dating through the synchronization of events according to Milankovitch cycles, casused by changes in the Earth''s orbit, and their expected effects on climate.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Pa231/U235',3,'U-Series Pa231/U235','19',15,'Dating based on the radioactive decay of uranium-series isotopes protactinium-231 and uranium-235.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Pb210',3,'Lead 210','19',15,'Dating based on the measurement of radioactive isotope lead-210, which is a product of uranium decay.');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Th230/U234',3,'U-Series Th230/U234','19',15,'Dating based on the radioactive decay of uranium-234 to thorium-230, and the degree to which secular equilibrium has been restored between these');
+insert into tbl_methods (method_abbrev_or_alt_name, method_group_id, method_name, record_type_id, unit_id, description) values ('Fis-Track',3,'Fission-Track','19',15,'Dating based on the trails left in some glassy minerals as a result of the radioactive decay of uranium.');
---
+++ sql/importChecks.sql
@@ -0,0 +1,189 @@
+﻿select 'tbl_sites' as t, site_name as n, count(site_name) as c from tbl_sites group by site_name having count(site_name) > 1
+union
+select 'tbl_biblio' as t, bugs_reference as n, count(bugs_reference) as c from tbl_biblio group by bugs_reference having count(bugs_reference) > 1
+union
+select 'tbl_contacts first_name' as t, first_name as n, count(first_name) as c from tbl_contacts group by first_name having count(first_name) > 1
+union
+select 'tbl_contacts last_name' as t, last_name as n, count(last_name) as c from tbl_contacts group by last_name having count(last_name) > 1
+union
+select 'tbl_locations country' as t, location_name as n, count(location_name) as c from tbl_locations 
+	where location_type_id = 1
+	group by location_name having count(location_name) > 1
+union
+select 'tbl_locations uncategorized' as t, location_name as n, count(location_name) as c from tbl_locations 
+	where location_type_id = 20
+	group by location_name having count(location_name) > 1
+union
+select 'tbl_geochronology' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+	  coalesce(dating_lab_id, 0) ||
+	  coalesce(lab_number, '')||
+	  coalesce(age, 0) ||
+	  coalesce(error_older, 0) ||
+	  coalesce(error_younger, 0) ||
+	  coalesce(delta_13c, 0) ||
+	  coalesce(notes, '')||
+	  coalesce(dating_uncertainty_id, 0) as compressedColumns
+	  from tbl_geochronology) as compressedTable
+	group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_analysis_entities unique sample - dataset combo' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+  from (
+	 select physical_sample_id || '-' || dataset_id as compressedColumns
+	 from tbl_analysis_entities
+	) as compressedTable
+  group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_physical_samples' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		sample_group_id || '-' || 
+		sample_name ||
+		sample_type_id as compressedColumns
+		from tbl_physical_samples
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_rdb' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		location_id || '-' || 
+		coalesce(rdb_code_id, 0) ||
+		taxon_id as compressedColumns
+		from tbl_rdb
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_rdb_codes' as t, rdb_category as n, count(rdb_category)
+	from tbl_rdb_codes
+	where tbl_rdb_codes is not null
+	group by rdb_category having count(tbl_rdb_codes) > 1
+union
+select 'tbl_relative_ages' as t, abbreviation as n, count(abbreviation) as c from tbl_relative_ages group by abbreviation having count(abbreviation) > 1
+union
+select 'tbl_sample_dimensions' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		physical_sample_id || '-' || 
+		dimension_id || '-' ||
+		method_id as compressedColumns
+		from tbl_sample_dimensions
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_sample_groups' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		site_id || '-' || 
+		sample_group_name as compressedColumns
+		from tbl_sample_groups
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_site_locations' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		location_id || '-' || 
+		site_id as compressedColumns
+		from tbl_site_locations
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_site_references' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		biblio_id || '-' || 
+		site_id as compressedColumns
+		from tbl_site_references
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_sites' as t, site_name as n, count(site_name) as c from tbl_sites group by site_name having count(site_name) > 1
+union
+select 'tbl_species_associations' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		associated_taxon_id || '-' || 
+		association_type_id || '-' ||
+		taxon_id as compressedColumns
+		from tbl_species_associations
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_taxa_common_names' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		common_name || 
+		language_id || '-' ||
+		taxon_id as compressedColumns
+		from tbl_taxa_common_names
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_taxa_measured_attributes' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		coalesce(attribute_measure, '') || 
+		coalesce(attribute_type, '') ||
+		coalesce(attribute_units, '') ||
+		data || '-' ||
+		taxon_id as compressedColumns
+		from tbl_taxa_measured_attributes
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_taxa_seasonality' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		activity_type_id || '-' ||
+		season_id || '-' ||
+		location_id || '-' ||
+		taxon_id as compressedColumns
+		from tbl_taxa_seasonality
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_taxa_tree' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		f.family_name || ' ' ||
+		g.genus_name || ' ' ||
+		s.species || ' ' ||
+		coalesce(a.author_name, '') as compressedColumns
+		from tbl_taxa_tree_master s
+		 left join tbl_taxa_tree_genera g on s.genus_id = g.genus_id
+		 left join tbl_taxa_tree_families f on g.family_id = f.family_id
+		 left join tbl_taxa_tree_authors a on s.author_id = a.author_id
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_taxonomic_order' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		taxonomic_order_system_id || '-' ||
+		taxonomic_code as compressedColumns
+		from tbl_taxonomic_order
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_taxonomy_notes' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		coalesce(taxonomy_notes, 'NULL') || ' ' || 
+		biblio_id || ' ' ||
+		taxon_id as compressedColumns
+		from tbl_taxonomy_notes
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_text_biology' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		coalesce(biology_text, 'NULL') || ' ' || 
+		biblio_id || ' ' ||
+		taxon_id as compressedColumns
+		from tbl_text_biology
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_text_distribution' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		coalesce(distribution_text, 'NULL') || ' ' ||
+		taxon_id as compressedColumns
+		from tbl_text_distribution
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
+union
+select 'tbl_text_identification_keys' as t, compressedTable.compressedColumns as n, count(compressedTable.compressedColumns)
+	from (select 
+		coalesce(key_text, 'NULL') || ' ' ||
+		taxon_id as compressedColumns
+		from tbl_text_identification_keys
+	 ) as compressedTable
+	 group by compressedTable.compressedColumns having count(compressedTable.compressedColumns) > 1
