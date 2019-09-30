248bea3a45d38099a2ac82112af52418db16e974
roger-mahler <roger.mahler@hotmail.com>
2019-09-30 20:00:10 +0200

    Initial import script

---
+++ run_import.bash
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+
+bugsdir=~/bugs
+tmpdir=~/$bugsdir/tmp
+dbhostfile=~/.default.sead.server
+dbuserfile=~/.default.sead.username
+dbtarget=sead_staging
+dbdeprecated=${dbtarget}_`date "+%Y%m%d%H%M%S"`
+on_conflict=no
+cr_version=@v0.1
+
+#echo "130.239.1.181" >  ~/.default.sead.server
+#echo "humlab_admin" >  ~/.default.sead.username
+
+if [[ -f "$dbhostfile" ]]; then
+    dbhost=`cat $dbhostfile`
+fi
+if [[ -f "$dbuserfile" ]]; then
+    dbuser=`cat $dbuserfile`
+fi
+
+if [[ ! -f $bugsdir ]]; then
+    mkdir $bugsdir
+fi
+
+rm -rf $bugsdir/tmp
+mkdir $bugsdir/tmp
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -h|--host)
+            dbhost="$2"; shift; shift
+        ;;
+        -u|--user)
+            dbuser="$2"; shift; shift
+        ;;
+        -t|--target-database)
+            dbtarget="$2"; shift; shift
+        ;;
+        -s|--source-datbase)
+            dbsource="$2"; shift; shift
+        ;;
+        -c|--clean-build)
+            on_conflict="$2"; shift
+        ;;
+        *)
+        POSITIONAL+=("$1")
+        shift
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ "$dbhost" != "130.239.1.181" ]; then
+    echo "This script can for now only be run on 130.239.1.181";
+    exit 64
+fi
+
+function build_from_source()
+{
+    cd $bugsdir
+    rm -rf $bugsdir/tmp && mkdir $bugsdir/tmp
+
+    git clone 'https://github.com/humlab-sead/sead_bugs_import.git'
+    cd sead_bugs_import
+    mvn clean package
+    cp ./target/*.jar $bugsdir/
+    cd $bugsdir && rm -rf ./tmp
+}
+# sudo apt-get install openjdk-8-jre-headless
+
+function download_latest_bugsdata()
+{
+    download_filename=bugsdata_`date "+%Y%m%d%H%M%S"`.zip
+    tempdir='./_temp_bugsimport'
+    if [[ -f $tempdir ]]; then
+        rm -rf $tempdir
+    fi
+    cd $datadir
+    wget -d --user-agent="Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0"  https://www.bugscep.com/downloads/bugsdata.zip
+    unzip bugsdata.zip bugsdata.mdb
+
+}
+
+mvn -Dmaven.test.skip=true clean package
+mkdir dist
+cp $BUGS_IMPORT_JAR ~/dist
+
+java -jar $BUGS_IMPORT_JAR --file=$bugsdata
+
